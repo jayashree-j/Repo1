e0f9b7ea04be7ae7de545b3d6770a7779b47c972
User <User@LAPTOP-QJUEPT6F>
2021-09-15 13:18:03 +0530

    Passenger added

---
+++ LTIAirlines/src/main/java/com/lti/model/Passenger.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Passenger {
+ int pass_no;
+
+public int getPass_no() {
+	return pass_no;
+}
+
+public void setPass_no(int pass_no) {
+	this.pass_no = pass_no;
+}
+ 
+}
